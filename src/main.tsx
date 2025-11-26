d431ba6105963686b989cd9057ac7efb99fd6ef6
turi.tibor <turi.tibor.1@gmail.com>
2025-11-26 23:40:40 +0100

    chore - adding tanstack

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import store from '@/store';
 import { initApp } from '@/store/actions/appActions';
 import IntlProviderWrapper from '@/components/providers/IntlProviderWrapper/IntlProviderWrapper';
 import MuiThemeProvider from '@/components/providers/MuiThemeProvider/MuiThemeProvider';
 import AppRoot from '@/components/providers/AppRoot/AppRoot';
 import './main.css';
+
+const queryClient = new QueryClient();
 
 store.dispatch(initApp());
 
@@ -15,12 +19,15 @@ if (!rootElement) throw new Error('Failed to find the root element');
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <MuiThemeProvider>
-        <IntlProviderWrapper>
-          <AppRoot />
-        </IntlProviderWrapper>
-      </MuiThemeProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <MuiThemeProvider>
+          <IntlProviderWrapper>
+            <AppRoot />
+          </IntlProviderWrapper>
+        </MuiThemeProvider>
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
